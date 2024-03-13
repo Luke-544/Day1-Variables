d5d7c0e6d1cd9d69b0962f8a007ee8af70e8a3c6
Luke-544 <lukesamuels422@gmail.com>
2024-03-13 21:10:52 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+first_name = "Samuel"
+last_name = "Mwaura"
+full_name = first_name +" "+ last_name
+print("Hello "+full_name)
+print(type(first_name))
+
+age = 20
+age+=1
+print(age)
+print(type(age))
+print("Your age is "+str(age)) #this is called type casting
+
+height = 120.2
+print(type(height))
+print("You are "+str(height)+"cms tall")
+
+human = True
+print(human)
+print(type(human))
